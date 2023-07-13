137a5564982bd77584944a61dd9339deadd1fe92
IvanArana <91325366+IvanArana@users.noreply.github.com>
2023-07-12 23:22:45 -0500

    Integracion Feature/id 48 49 50/carrito&datos user (#14)
    * id-43 detalles de productos
    
    * vista de carrito y datos de usuario junto con editar
    
    ---------
    
    Co-authored-by: Fernanda Villagran Hernández <villagrannn06@gmail.com>

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"root",
+    password:"2023",
     database:"wristwondersdb"
 })
 
