4e956979f2368b13ee5c79259cb5f185084e0fca
Ferchii5 <91686123+Ferchii5@users.noreply.github.com>
2023-07-15 16:22:26 -0500

    Feature/id 32/pantalla principal usuario (#15)
    * Integracion de API para dashboard ID-31
    
    * implementacion de las funciones de back para catalogo de productos y detalle de productos correspondientes al ID-32 & ID-33
    
    ---------
    
    Co-authored-by: IvanArana <iv4ng4mer01@gmail.com>
    Co-authored-by: IvanArana <91325366+IvanArana@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"2023",
+    host:"minerserver.servegame.com",
+    user:"admin",
+    password:"Panabolillo123",
     database:"wristwondersdb"
 })
 
@@ -77,12 +77,40 @@ app.get("/categorias/count", (req, res) => {
     );
 });
 
-/**
- * *Area de trabajo de Soto
- */
+app.get("/productos", (req, res) => {
+    db.query(
+      'SELECT p.*, tr.nombre_tipo, m.nombre_marca, ma.nombre_material FROM producto p JOIN tipo_reloj tr ON p.tipo_reloj = tr.idtipo_reloj JOIN marca m ON p.marca = m.idmarca JOIN material ma ON p.material = ma.idmaterial',
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  });
 
+// ...
 
-
-/**
- * *Area de trabajode Hannia
- */
+app.get("/productos/:id", (req, res) => {
+    const { id } = req.params;
+    db.query(
+      'SELECT p.*, tr.nombre_tipo, m.nombre_marca, ma.nombre_material FROM producto p JOIN tipo_reloj tr ON p.tipo_reloj = tr.idtipo_reloj JOIN marca m ON p.marca = m.idmarca JOIN material ma ON p.material = ma.idmaterial WHERE p.idproducto = ?',
+      [id],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send('Error en el servidor');
+        } else {
+          if (result.length > 0) {
+            const producto = result[0];
+            res.send(producto);
+          } else {
+            res.status(404).send('Producto no encontrado');
+          }
+        }
+      }
+    );
+  });
+  
+  // ...
